9234550da3c6d0e8df384805dd226081e4464494
Dudous <sampaioeedaurdo36@gmail.com>
2024-09-05 21:18:09 -0300

    API FUNCIONANDO!

--- public/js/registerCar.js
+++ public/js/registerCar.js
@@ -1,4 +1,6 @@
-alert('test')
+marca = $("#carBrand")
+modelo = $("#carModel")
+ano = $("#carYear")
 
 // URL do endpoint de login
 const loginUrl = 'https://cluster.apigratis.com/api/v2/login';
@@ -29,7 +31,7 @@ async function getToken() {
         }
         const data = await response.json();
         // Substitua 'token' pelo nome correto do campo que contém o token
-        console.log(data); 
+        return(data.authorization.token); 
     } catch (error) {
         console.error('Erro ao obter o token:', error);
     }
@@ -46,13 +48,9 @@ getToken().then(token => {
 // URL do endpoint de dados do veículo
 const vehicleDataUrl = 'https://cluster.apigratis.com/api/v2/vehicles/dados';
 
-// Dados do veículo
-const vehicleData = {
-    placa: 'OBG2F55'
-};
-
 // Função para requisitar dados do veículo
-async function fetchVehicleData(token) {
+async function fetchVehicleData(token, plate) {
+    
     try {
         const response = await fetch(vehicleDataUrl, {
             method: 'POST',
@@ -61,25 +59,27 @@ async function fetchVehicleData(token) {
                 'Authorization': `Bearer ${token}`,
                 'DeviceToken': 'b05da8fa-524d-402e-86b8-b4c4633a368a' // Substitua com o token real do dispositivo
             },
-            body: JSON.stringify(vehicleData)
+            body: JSON.stringify({placa: $('#carPlate').val()})
         });
         if (!response.ok) {
             throw new Error('Failed to fetch vehicle data: ' + response.statusText);
         }
         const data = await response.json();
         console.log('Dados do veículo:', data);
+        marca.val(data.response.MARCA)
+        modelo.val(data.response.MODELO)
+        ano.val(data.response.ano)
     } catch (error) {
         console.error('Erro ao requisitar dados do veículo:', error);
     }
 }
 
 // Função principal para autenticar e obter dados do veículo
-async function main() {
-    const token = await getToken();
-    if (token) {
-        await fetchVehicleData(token);
-    }
-}
+// async function main() {
+//     const token = await getToken();
+//     if (token) {
+//         await fetchVehicleData(token);
+//     }
+// }
 
-// Executa a função principal
-main();
+
